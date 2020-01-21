1befb6d9c233af9f76f8fbf4cd77774fc3d248cb
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2020-01-21 23:15:35 +0100

    Changes in c405.php, added setAllow function

--- src/c405.php
+++ src/c405.php
@@ -6,6 +6,17 @@ class c405 extends ResponseStatus {
     public function getCode() : int {
         return 405;
     }
+
+    public function setAllow(array $allow) {
+        if(count($allow) > 0) {
+            $header = "Allow: ";
+            foreach($allow as $a) {
+                $header = $header.$a.", ";
+            }
+            $header = \substr($header,0,\strlen($header) - 2);
+            $this->headers[] = $header;
+        }
+    }
 }
 
 ?>
